24694ef3ccbc898e3b0709ea932aad22c125312b
sudo-del-System32 <franciscodanielbertinidealmeida@yahoo.com.br>
2025-12-30 21:04:36 -0300

    refactor: all variables s to g

--- src/Bibliotecas/garrafas.h
+++ src/Bibliotecas/garrafas.h
@@ -8,5 +8,8 @@ int qnt;
 //Funçao de verificar se a garrafa esta vazia.
 bool vazia(garrafa *g);
 
+//Função de verificar se a garrafa esta cheia.
+bool cheia(garrafa *s);
+
 //Funçao de retirar um item, retorna o item retirado.
 char *pop(garrafa *g);
